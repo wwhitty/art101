5b6b02e9b01e5fed9759d8f2af5848bd9bbf911e
wwhitty <whittywill@gmail.com>
2021-10-18 23:47:07 -0700

    Lab 5 complete
    finished lab 5, needs more css though

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Will Whitty <wwhitty@ucsc.edu>
+ * Created: 18 October
+ * License: Public Domain
+ */
+
+// Define Variables
+make = "Tesla";
+model = "Model X";
+color = "White";
+year = "2018";
+
+//calculate
+age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
